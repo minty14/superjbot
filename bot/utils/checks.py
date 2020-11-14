e6647c6c0e1dac9b5b16dcd12d4d4876f359a396
David Minty <>
2020-11-14 19:43:40 +0000

    Fixed an issue with checks running in DMs

--- bot/utils/checks.py
+++ bot/utils/checks.py
@@ -3,7 +3,10 @@ Custom commands.check functions
 
 Defines custom checks to be passed to @commands.check()
 """
+from settings.constants import GUILD_ID
 
 # Checks that the author of the invoked command is in a user group higher than @everyone
 def is_admin(ctx):
-    return any(role > ctx.guild.default_role for role in ctx.author.roles)
+    guild = ctx.bot.get_guild(GUILD_ID)
+    author = guild.get_member(ctx.author.id)
+    return any(role > guild.default_role for role in author.roles)
